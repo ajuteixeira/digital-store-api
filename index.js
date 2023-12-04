a4448ea8ec5ce82dc60a5cbe290cff48809a4f6b
Juliana Teixeira <teixeirajuuh@gmail.com>
2023-12-04 17:06:03 -0300

    corrigindo bug e identação

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express"); //pega tudo que tem no pacote do express e coloca nessa variável
 const cors = require("cors");
-const brandRoutes = require('./routes/brandRoutes')
+const brandRoutes = require("./routes/brandRoutes");
 
 const app = express();
 const port = 8000;
@@ -8,12 +8,12 @@ const port = 8000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Bem-vindo à API da Digital Store");
+app.get("/", (req, res) => {
+  res.send("Bem-vindo à API da Digital Store");
 });
 
-app.use('/marcas', brandRoutes) // recebe dois parametros: base da rota e o arquivo importado
+app.use("/marcas", brandRoutes); // recebe dois parametros: base da rota e o arquivo importado
 
 app.listen(port, () => {
-    console.log(`Servidor de pé na url: http://localhost:${port}`);
+  console.log(`Servidor de pé na url: http://localhost:${port}`);
 });
--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -1,20 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/marcas', (req, res) => {
-    res.send('lista de marcas');
+router.get("/", (req, res) => {
+  res.send("lista de marcas");
 });
-
-router.post('/marcas', (req, res) => {
-    res.send(`cria uma marca: ${JSON.stringify.req.body}`);
+router.post("/", (req, res) => {
+  res.send(`cria uma marca: ${JSON.stringify(req.body)}`);
 });
-
-router.post('/marcas/:id', (req, res) => {
-    res.send(`edita a marca de id ${req.params.id}`);
+router.post("/:id", (req, res) => {
+  res.send(`edita a marca de id ${req.params.id}`);
 });
-
-router.delete('/marcas/:id', (req, res) => {
-    res.send(`deleta a marca de id ${req.params.id}`);
+router.delete("/:id", (req, res) => {
+  res.send(`deleta a marca de id ${req.params.id}`);
 });
 
 module.exports = router; //exporta pra usar em outro arquivo
